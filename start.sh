d959e9130d3b95cba812c32bfb2d7a0edeb832af
Rick van der Staaij <rvdstaaij@enrise.com>
2020-07-13 13:33:28 +0000

    Replace tabs with spaces

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 echo "Starting development hosts proxy..."
 docker network create enrise-dev-proxy > /dev/null 2>&1 || true
 (docker run \
-	-d \
-	--rm \
-	-p 80:80 \
-	-p 8080:8080 \
-	-v /var/run/docker.sock:/var/run/docker.sock:ro \
-	--name enrise-dev-proxy \
-	--network enrise-dev-proxy \
-	traefik:v2.2 \
-	--api.insecure=true \
-	--providers.docker=true \
-	--providers.docker.exposedbydefault=false \
-	--entrypoints.web.address=:80 > /dev/null \
-	|| true) && echo "Development hosts proxy is running."
+    -d \
+    --rm \
+    -p 80:80 \
+    -p 8080:8080 \
+    -v /var/run/docker.sock:/var/run/docker.sock:ro \
+    --name enrise-dev-proxy \
+    --network enrise-dev-proxy \
+    traefik:v2.2 \
+    --api.insecure=true \
+    --providers.docker=true \
+    --providers.docker.exposedbydefault=false \
+    --entrypoints.web.address=:80 > /dev/null \
+    || true) && echo "Development hosts proxy is running."

7dd6b7558be42e1d362c7cfe7daa6ba44f87e66c
Stefan van Essen <git@stefan-van-essen.nl>
2020-07-16 17:35:16 +0200

    Nicer output and port change
    By changing the port to 10080 other tools like debugging proxies can
    still run on port 8080 in conjunction with this dev proxy.

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
-echo "Starting development hosts proxy..."
+if ($(docker ps | grep -q enrise-dev-proxy)); then
+  echo "Development hosts proxy is already running."
+  exit 0
+fi
+
+echo -n "Starting development hosts proxy... "
 docker network create enrise-dev-proxy > /dev/null 2>&1 || true
 (docker run \
     -d \
     --rm \
     -p 80:80 \
-    -p 8080:8080 \
+    -p 10080:10080 \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     --name enrise-dev-proxy \
     --network enrise-dev-proxy \
@@ -12,5 +17,5 @@ docker network create enrise-dev-proxy > /dev/null 2>&1 || true
     --api.insecure=true \
     --providers.docker=true \
     --providers.docker.exposedbydefault=false \
-    --entrypoints.web.address=:80 > /dev/null \
-    || true) && echo "Development hosts proxy is running."
+    --entrypoints.web.address=:80 \
+    --entrypoints.traefik.address=:10080 > /dev/null) && echo -e "[\e[92mOK\e[39m]"
